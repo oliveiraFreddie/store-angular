0426c7cd9dab580bfecb6d344a7b3e5f75ed87c0
Frederico Oliveira <freddie.oliveira95@gmail.com>
2024-02-15 10:15:32 +0000

    product-header/filters

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,20 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 
+const ROWS_HEIGHT: { [id:number]: number } = {1: 400, 3: 335, 4: 350};
+
+
 @Component({
   selector: 'app-home',
-  template: `
-    <p>
-      home works!
-    </p>
-  `,
-  styles: [
-  ]
+  templateUrl: './home.component.html' 
 })
 export class HomeComponent implements OnInit {
-
+  cols = 3;
+  rowHeight = ROWS_HEIGHT[this.cols];
+  category: string | undefined;
+  
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onColumnsCountChange(colsNum: number): void {
+    this.cols = colsNum;
+    this.rowHeight = ROWS_HEIGHT[this.cols];
+  }
+
+  onShowCategory(newCategory: string): void {
+    this.category = newCategory;
   }
 
 }
